9a3556a033aa8afff6ed98060473c8cb0f7435df
marcosprofile <marcos_laurindo1997@hotmail.com>
2023-07-23 00:48:25 -0300

    Iniciando criação e estilização dos componentes

---
+++ src/app/shared/card-depoimentos/card-depoimentos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-depoimentos',
+  templateUrl: './card-depoimentos.component.html',
+  styleUrls: ['./card-depoimentos.component.scss']
+})
+export class CardDepoimentosComponent {
+
+}
