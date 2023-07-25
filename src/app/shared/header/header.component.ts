d4f62cb3255938e21839d868a503b4b20523c21f
marcosprofile <marcos_laurindo1997@hotmail.com>
2023-07-24 23:54:40 -0300

    update responsive header + modal

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +7,50 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  /* isNavbarVisible = false;
+  iconStatus = 'menu';
+  screenWidth: number;
+
+  constructor() {
+    this.screenWidth = window.innerWidth;
+    this.updateNavbarVisibility();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.screenWidth = event.target.innerWidth;
+    this.updateNavbarVisibility();
+  }
+
+  updateNavbarVisibility() {
+    this.isNavbarVisible = this.screenWidth <= 500;
+  }
+
+  toggleNavbar() {
+    this.isNavbarVisible = !this.isNavbarVisible;
+    this.iconStatus = this.isNavbarVisible ? 'close' : 'menu';
+  } */
+
+  isNavbarVisible = false;
+  iconStatus = 'menu';
+  screenWidth: number;
+
+  constructor() {
+    this.screenWidth = window.innerWidth;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.screenWidth = event.target.innerWidth;
+    this.updateNavbarVisibility();
+  }
+
+  updateNavbarVisibility() {
+    this.isNavbarVisible = this.screenWidth <= 700;
+  }
+
+  toggleNavbar() {
+    this.isNavbarVisible = !this.isNavbarVisible;
+    this.iconStatus = this.isNavbarVisible ? 'close' : 'menu';
+  }
 }
