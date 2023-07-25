47c6bd2556bb57726a23e3e2900f3d09252ebc3c
marcosprofile <marcos_laurindo1997@hotmail.com>
2023-07-25 00:13:27 -0300

    update header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,42 +1,20 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
-
-  /* isNavbarVisible = false;
-  iconStatus = 'menu';
-  screenWidth: number;
-
-  constructor() {
-    this.screenWidth = window.innerWidth;
-    this.updateNavbarVisibility();
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onResize(event: any) {
-    this.screenWidth = event.target.innerWidth;
-    this.updateNavbarVisibility();
-  }
-
-  updateNavbarVisibility() {
-    this.isNavbarVisible = this.screenWidth <= 500;
-  }
-
-  toggleNavbar() {
-    this.isNavbarVisible = !this.isNavbarVisible;
-    this.iconStatus = this.isNavbarVisible ? 'close' : 'menu';
-  } */
+export class HeaderComponent implements OnInit {
 
   isNavbarVisible = false;
   iconStatus = 'menu';
-  screenWidth: number;
+  screenWidth: number = window.innerWidth;
 
-  constructor() {
-    this.screenWidth = window.innerWidth;
+  @ViewChild('navbarElement', { static: true }) navbarElement!: ElementRef;
+
+  ngOnInit() {
+    this.updateNavbarVisibility();
   }
 
   @HostListener('window:resize', ['$event'])
@@ -46,11 +24,18 @@ export class HeaderComponent {
   }
 
   updateNavbarVisibility() {
-    this.isNavbarVisible = this.screenWidth <= 700;
+    this.isNavbarVisible = this.screenWidth <= 600;
+    const navbarEl = this.navbarElement.nativeElement as HTMLElement;
+    navbarEl.style.height = this.isNavbarVisible ? '274px' : '76px';
+    navbarEl.style.display = this.isNavbarVisible ? 'flex' : 'none';
   }
 
   toggleNavbar() {
     this.isNavbarVisible = !this.isNavbarVisible;
     this.iconStatus = this.isNavbarVisible ? 'close' : 'menu';
+
+    const navbarEl = this.navbarElement.nativeElement as HTMLElement;
+    navbarEl.style.display = this.isNavbarVisible ? 'flex' : 'none';
+    navbarEl.style.height = this.isNavbarVisible ? '274px' : '76px';
   }
 }
