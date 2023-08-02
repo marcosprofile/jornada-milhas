da1f2b8775a702844ccbce1a37de81ae025dcb1c
marcosprofile <marcos_laurindo1997@hotmail.com>
2023-08-01 22:02:37 -0300

    Create props components cards

--- src/app/shared/card-busca/card-busca.component.ts
+++ src/app/shared/card-busca/card-busca.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-busca',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-busca.component.scss']
 })
 export class CardBuscaComponent {
-
+  @Input() imagem: string = '';
+  @Input() cidade: string = '';
+  @Input() preco: string = '';
 }
--- src/app/shared/card-depoimentos/card-depoimentos.component.ts
+++ src/app/shared/card-depoimentos/card-depoimentos.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-depoimentos',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-depoimentos.component.scss']
 })
 export class CardDepoimentosComponent {
-
+  @Input() imagem: string = '';
+  @Input() depoimento: string = '';
+  @Input() nome: string = '';
 }
